ff7ebca2271df3bde50a60375d0ddf13517fe436
Aditya yadav <83342917+adi3120@users.noreply.github.com>
2022-10-28 22:44:51 +0530

    Update mouseTrail.cpp

--- mouseTrail.cpp
+++ mouseTrail.cpp
@@ -8,15 +8,15 @@ using namespace std;
 
 int main(){
 	Fazen2d game;
-    game.make2DConsole(16,16);
-	game.background(whiteB);
+    	game.make2DConsole(16,16);
+	game.background();
 	
 	vector<pair<float,float>> v;
 	int n=100;
 
     while (1)
     {
-		game.background(whiteB);
+		game.background();
 
 		v.push_back(make_pair(game.Mouse_X(),game.Mouse_Y()));
 
@@ -25,6 +25,9 @@ int main(){
 		}
 		for(float i=0;i<v.size();i+=1){
 			pair<float,float> curr=v[i];
+			
+			// For coloured trails
+			
 			if(i<n/6)
 				game.drawRectangle(curr.first,curr.second,1,1,L'*',0,dmagentaF);
 			else if(i<2*(n/6) && i>n/6)
@@ -37,6 +40,8 @@ int main(){
 				game.drawCircle(curr.first,curr.second,3,L'&',dyellowF,true,dyellowF);
 			else if(i>5*(n/6))
 				game.drawCircle(curr.first,curr.second,4,L'#',redF,true,redF);
+			
+			//For white trails
 
 			// if(i<n/6)
 			// 	game.drawRectangle(curr.first,curr.second,1,1,L'*');
